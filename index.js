9d97435c8eb02468bfab329d4ae0c1e20068888c
sjagoori <sjagoori@users.noreply.github.com>
2020-05-10 22:53:11 +0200

    created button component

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const indexRouter = require('./routes/indexRouter');
 
 app.set('view engine', 'ejs');
-app.use('/express', indexRouter);
+app.use(express.static(__dirname + '/public'));
+
+// app.use('/express', indexRouter);
+app.use('/', indexRouter);
 
 app.listen(port);
